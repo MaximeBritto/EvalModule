f1cee1674cde47b18192471a328bec66fc9a45e0
Maxime <maxime.britto@edu.devinci.fr>
2023-11-21 14:45:03 +0100

    presqueFiniDarkMode

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      extend: {
+        borderRadius: {
+          btnRadius: "50%",
+        },
+        colors: {
+          "blackBg": "#141414",
+          
+          "whiteBg": {
+            100: "#F7F7F7",
+            200: "#FFFFFF",
+          },
+        },
+      },
+  },
+  plugins: [],
+  darkMode: "class",
+};
+
